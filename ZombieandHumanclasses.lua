eb613bf0f2309f40937d41195de1cb006522f56d
Mattymatt1 <166906010+Mattymatt1@users.noreply.github.com>
2025-05-28 13:29:52 -0500

    Create ZombieandHumanclasses.lua

---
+++ ZombieandHumanclasses.lua
@@ -0,0 +1,25 @@
+local zombie = { -- Zombie class
+ health = 90,
+ AttackingHumans = true,
+ IQ = 80
+}
+
+local human = { -- Human class
+ health = 100,
+ AttackingHumans = false,
+ IQ = 95
+}
+
+function zombie:create(name) -- Creating new zombie classes
+ self.name = name
+ return self
+end
+
+function human:create(name)
+ self.name = name
+ return self
+end
+
+
+-- test
+local matthew = human:create("Matthew")
